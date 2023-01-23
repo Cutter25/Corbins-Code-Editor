c85eb1257ffac9bffc57cf20a946b2fd3344dfb6
Corbin Cutter <Corbin@Lisas-iMac.lan>
2023-01-22 22:06:43 -0500

    Db.js done

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,17 +13,46 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
+
 export const putDb = async (content) => {
-  console.error('PUT to the database');
+
+  // edited to console.log what method I am using lol
+
+  console.log('PUT to the database');
+
+  // Connect to the database created above
+
   const contactDb = await openDB('jate', 1);
+
+  // Create a transaction and specify the permissions. In this case with a put, I want to wread and write
+
   const tx = contactDb.transaction('jate', 'readwrite');
+
+  // Open the store up
+
   const store = tx.objectStore('jate');
+
+  // Putting content into the database
+
   const request = store.put({ id: 1, value: content });
+
+  // Confirming what I want to happen, happened
+
   const result = await request;
   console.log('🚀 - data saved to the database', result);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getAllDb = async () => console.error('getDb not implemented');
+
+export const getAllDb = async () => {
+  console.log('getAllDb trying to work!');
+  const contactDb = await openDB('jate', 1);
+  const tx = contactDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  return result?.value;
+};
 
 initdb();
